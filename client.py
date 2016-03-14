89f2aca937b2d47d7010bf575f5a7b31b8c38fb7
Jeremy Price <jprice.rhit@gmail.com>
2016-03-14 18:17:14 -0500

    flake8 compliant now!

--- client.py
+++ client.py
@@ -1,42 +1,41 @@
 #!/usr/bin/env python2
 
 import requests
-import sys
 import logging
 
+
 def debug_mode():
-    try: # for Python 3
+    try:  # for Python 3
         from http.client import HTTPConnection
     except ImportError:
         from httplib import HTTPConnection
     HTTPConnection.debuglevel = 1
 
-    logging.basicConfig() # you need to initialize logging, otherwise you will not see anything from requests
+    logging.basicConfig()
     logging.getLogger().setLevel(logging.DEBUG)
     requests_log = logging.getLogger("requests.packages.urllib3")
     requests_log.setLevel(logging.DEBUG)
     requests_log.propagate = True
 
 
-#debug_mode()
-#TODO: pull this in from command line
-url = "http://localhost:5000/"
+# debug_mode()
+url = "http://localhost:5000/"  # TODO: pull this in from command line
 
 # first request
 first_hit = requests.get(url)
 first_json = first_hit.json()
 token = first_json['token']
 next_url = first_json['next_url']
-print "My ID is {}".format(token)
+print("My ID is {}".format(token))
 
 # subsequent requests
 done = False
 while not done:
-    print "Accessing {}".format(next_url)
+    print("Accessing {}".format(next_url))
     hit = requests.post(next_url, json={'token': token})
     response = hit.json()
     if 'answer' in response:
-        print response
+        print(response)
         done = True
     else:
         next_url = response.popitem()[1]
